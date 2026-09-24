792f92e466bfce7b0ef79fb430716fe8f51833eb
Mika <mikagroth555@gmail.com>
2026-09-24 17:58:22 +0200

    finished maze generator

--- core/src/main/java/com/maze/MazeGenerator.java
+++ core/src/main/java/com/maze/MazeGenerator.java
@@ -7,31 +7,39 @@ import java.util.Stack;
 
 public class MazeGenerator {
 
-    public String[] generateLevel(int rows, int columns, int seed){
+    public String[] generateLevel(int rows, int columns, int seed) {
         Random random = new Random(seed);
-        if (!validateSize(columns,rows)){
+        if (!validateSize(columns, rows)) {
             throw new IllegalArgumentException("Size is invalid!!");
         }
         String[] level;
         char[][] layout = new char[rows][columns];
         boolean[][] visited = new boolean[rows][columns];
         Stack<Cell> stack = new Stack<Cell>();
-        Cell start = new Cell(1,1);
+        Cell start = new Cell(1, 1);
         stack.push(start);
         visited[start.row()][start.column()] = true;
         layout[start.row()][start.column()] = '.';
-        while(!stack.isEmpty()){
-            Cell current;
-            current = stack.peek();
-
+        while (!stack.isEmpty()) {
+            Cell current = stack.peek();
+            List<Cell> neighbours = unvisitedNeighbors(current,visited,rows,columns);
+            if(neighbours.isEmpty()){
+                stack.pop();
+                continue;
+            }
+            Cell next = neighbours.get(random.nextInt(neighbours.size()));
+            carve(layout, current, next);
+            visited[next.row()][next.column()] = true;
+            stack.push(next);
         }
-        return null;
+        return toStringMap(layout);
     }
-    public boolean validateSize(int columns, int rows){
-        if(rows < 1 || columns < 1){
+
+    public boolean validateSize(int columns, int rows) {
+        if (rows < 1 || columns < 1) {
             return false;
         }
-        if(rows > 20 || columns > 20){
+        if (rows > 20 || columns > 20) {
             return false;
         }
         return true;
@@ -64,16 +72,33 @@ public class MazeGenerator {
                             + direction[1];
 
             if (row <= 0
-                            || row >= rows - 1
-                            || column <= 0
-                            || column >= columns - 1
+                    || row >= rows - 1
+                    || column <= 0
+                    || column >= columns - 1
             ) {
                 continue;
             }
 
-            if (!visited[row][column]) { neighbors.add( new Cell( row, column));
+            if (!visited[row][column]) {
+                neighbors.add(new Cell(row, column));
             }
         }
         return neighbors;
+    }
+
+    private void carve(char[][] layout, Cell current, Cell next){
+        int pathRow = (current.row() + next.row())/2;
+        int pathColumn = (current.column() + next.column())/2;
+        layout[pathRow][pathColumn] = '.';
+        layout[next.row()][next.column()] = '.';
+    }
+    private String[] toStringMap(char[][] layout){
+        //[[#.#.###]
+         //[#...###]]
+        String[] result = new String[layout.length];
+        for(int row = 0; row <= layout.length; row++){
+            result[row] = new String(layout[row]);
+        }
+        return result;
     }
 }
